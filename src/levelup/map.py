ed6b1648b39d3af61450afb8d593cd47cd941a2f
Kedar <kedarnj@gmail.com>
2023-01-26 20:56:50 +0000

    Add more map stuff

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from levelup.position import Position
+from levelup.controller import Direction
 
 class direction(Enum):
     EAST = "e"
@@ -7,6 +9,23 @@ class direction(Enum):
     SOUTH = "s"
 
 class GameMap:
-    def get_total_positions(self):
-        return 100
 
+    position_list: list 
+    num_positions: int
+
+    def __init__(self):
+        self.position_list = []
+        for x in range(0, 10):
+            for y in range(0, 10):
+                self.position_list.append(Position(x, y))
+        self.num_positions = len(self.position_list)
+                    
+    def get_total_positions(self):
+        return self.num_positions
+
+    def get_positions(self):
+        return self.position_list         
+
+    def calculate_position(self, starting_position: Position, direction: Direction):
+        print(starting_position)
+        return Position(5,6)
--- tests/test_game_map.py
+++ tests/test_game_map.py
@@ -1,7 +1,19 @@
 from unittest import TestCase
 from levelup.map import GameMap
+from levelup.position import Position
+from levelup.controller import Direction
 
 class TestGameMap(TestCase):
     def test_get_total_positions(self):
         test_gamemap = GameMap()
         self.assertEqual(100, test_gamemap.get_total_positions())
+
+    def test_get_positions(self):
+        test_gamemap = GameMap()
+        self.assertEqual(100, len(test_gamemap.get_positions()))
+
+    def test_calculate_position_mid(self):
+        test_gamemap = GameMap()
+        starting_position = Position(5,5)
+        expected_position = Position(5,6)
+        self.assertEqual(expected_position, test_gamemap.calculate_position(starting_position, Direction.NORTH))
