9d8d1eb8f576b7824bb59261d68a8859b6cf62d7
Andrew & Tom <andrewmallamo@gmail.com>
2023-01-26 19:46:32 +0000

    Adding test_character.py

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,1 +1,15 @@
+from unittest import TestCase
+from levelup.character import Character, DEFAULT_POSITION
 
+class TestCharacter(testCase):
+    def test_init(self):
+        fake_name = "Balthazar"
+        test_character = Character(fake_name)
+        self.assertEqual(test_character.name, fake_name)
+        self.assertEqual(test_character.current_position, DEFAULT_POSITION)
+
+
+
+ 
+    
+        
