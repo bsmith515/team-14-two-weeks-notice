6be89ac02a728bfcded4fda91bc18a508475683e
Andrew & Tom <andrewmallamo@gmail.com>
2023-01-26 20:50:50 +0000

    enterMap test added

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -2,13 +2,18 @@ from dataclasses import dataclass
 from levelup.position import Position
 
 DEFAULT_POSITION = Position(0, 0)
+DEFAULT_CHARACTER_NAME = "Bubba"
 
 @dataclass
 class Character:
     name: str
     current_position: Position
     
-    def __init__(self, name):
+    def __init__(self, name=DEFAULT_CHARACTER_NAME):
         self.name = name
         self.current_position = DEFAULT_POSITION
+
+    def getName(self):
+        return self.name
+
 
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,12 +1,33 @@
 from unittest import TestCase
-from levelup.character import Character, DEFAULT_POSITION
+from levelup.character import Character, DEFAULT_POSITION, DEFAULT_CHARACTER_NAME
+from levelup.map import GameMap
 
 class TestCharacter(TestCase):
-    def test_init(self):
+    def test_init_given_name(self):
         fake_name = "Balthazar"
         test_character = Character(fake_name)
         self.assertEqual(test_character.name, fake_name)
-        self.assertEqual(test_character.current_position, DEFAULT_POSITION)
+ 
+    def test_init_given_no_name(self):
+        test_character = Character()
+        self.assertEqual(test_character.name, DEFAULT_CHARACTER_NAME)
+ 
+    def test_get_Name(self):
+        fake_name = "Tiny"
+        test_character = Character(fake_name)
+        test_name = test_character.getName()
+        self.assertEqual(test_name, fake_name)
+
+    def enter_map(self):
+       test_character = Character()
+       test_game_map = GameMap()
+       test_character.enterMap(test_game_map)
+
+       self.assertIsNotNone(test_character.current_position)
+
+       
+
+
 
 
 
